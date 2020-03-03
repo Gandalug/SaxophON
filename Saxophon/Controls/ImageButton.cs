0358e81cdc5538ac0fc36b9d5567dba7f78511a0
Robin Rauchmann <Robin.Rauchmann@prodot.de>
2020-03-03 15:58:11 +0100

    Add Style

---
+++ Saxophon/Controls/ImageButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Saxophon.Controls
+{
+    public class ImageButton : Button
+    {
+        static ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+            nameof(Content), typeof(string), typeof(ImageButton), new PropertyMetadata(default(string)));
+
+        public string Content
+        {
+            get => (string) GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
+            nameof(ImageSource), typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(default(ImageSource)));
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource) GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+    }
+}
--- Saxophon/ViewModels/MainWindowViewModel.cs
+++ Saxophon/ViewModels/MainWindowViewModel.cs
@@ -7,10 +7,34 @@ namespace Saxophon.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        private bool _isMessageVisible;
+        private string _popupText;
         private ObservableCollection<NoteViewModel> _notes = new ObservableCollection<NoteViewModel>();
         public MainWindowViewModel()
         {
             AddNoteCommand = new RelayCommand(ExecuteAddNoteCommand, CanExecuteAddNoteCommand);
+            DeleteNoteCommand = new RelayCommand(ExecuteDeleteNoteCommand, CanExecuteDeleteNoteCommand);
+            SaveCommand = new RelayCommand(ExecuteSaveCommand, CanExecuteSaveCommand);
+        }
+        
+        public bool IsMessageVisible
+        {
+            get => _isMessageVisible;
+            set
+            {
+                _isMessageVisible = value;
+                OnPropertyChanged(nameof(IsMessageVisible));
+            }
+        }
+        
+        public string PopupText
+        {
+            get => _popupText;
+            set
+            {
+                _popupText = value;
+                OnPropertyChanged(nameof(PopupText));
+            }
         }
 
         public ObservableCollection<NoteViewModel> Notes
@@ -32,8 +56,34 @@ namespace Saxophon.ViewModels
 
         private void ExecuteAddNoteCommand(object parameter)
         {
-            var image = new BitmapImage(new Uri("pack://application:,,,/Saxophon;component/Resources/horns.png"));
-            Notes.Add(new NoteViewModel{ Name = "c", Image = image});
+            var image = new BitmapImage(new Uri("pack://application:,,,/Saxophon;component/Resources/Add_16x.png"));
+            Notes.Add(new NoteViewModel{ Name = "a", Image = image});
+        }
+
+        public RelayCommand DeleteNoteCommand { get; set; }
+
+        private bool CanExecuteDeleteNoteCommand(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecuteDeleteNoteCommand(object parameter)
+        {
+            var image = new BitmapImage(new Uri("pack://application:,,,/Saxophon;component/Resources/Cancel_16x.png"));
+            Notes.Add(new NoteViewModel{ Name = "d", Image = image});
+        }
+        public RelayCommand SaveCommand { get; set; }
+
+        private bool CanExecuteSaveCommand(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecuteSaveCommand(object parameter)
+        {
+            IsMessageVisible = false;
+            PopupText = "Note sheet was created";
+            IsMessageVisible = true;
         }
     }
 }
